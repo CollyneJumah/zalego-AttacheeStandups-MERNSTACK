bd79a6c81747c3bb867593aaeb1f7cce8428e561
CollyneJumah <collynejumah2010@gmail.com>
2021-03-18 16:07:51 +0300

    Created react components

---
+++ src/defaultlayouts/DefaultLayouts.js
@@ -0,0 +1,14 @@
+import Header from './Header'
+import Footer from './Footer'
+
+const DefaultLayouts = () => {
+    
+    return (
+       <div className="default-layout">
+            <Header />
+            <Footer />
+       </div>
+    )
+}
+
+export default DefaultLayouts
